c0803053686f276206e92d146c370ac5e31d87ed
tao <xaybundith1168@gmail.com>
2016-12-14 17:12:27 +0700

    finish

--- app/src/main/java/appewtc/masterung/whereltc/MainActivity.java
+++ app/src/main/java/appewtc/masterung/whereltc/MainActivity.java
@@ -6,12 +6,18 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
     //Explicit
     private EditText userEditText, passwordEditText;
     private String userString, passwordString;
+    private String[] loginString;
+    private boolean aBoolean = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +52,52 @@ public class MainActivity extends AppCompatActivity {
                 synUser.execute();
                 String s = synUser.get();
                 Log.d("14decV2", "JSoN ==> " + s);
+
+                JSONArray jsonArray = new JSONArray(s);
+
+                loginString = new String[4];
+
+                for (int i = 0; i < jsonArray.length(); i += 1) {
+
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                    if (userString.equals(jsonObject.getString("User"))) {
+                        loginString[0] = jsonObject.getString("id");
+                        loginString[1] = jsonObject.getString("Name");
+                        loginString[2] = jsonObject.getString("User");
+                        loginString[3] = jsonObject.getString("Password");
+                        aBoolean = false;
+                    }
+
+                }  // for
+
+
+                if (aBoolean) {
+
+                    MyAlert myAlert = new MyAlert(MainActivity.this,
+                            getResources().getString(R.string.title_user_fale),
+                            getResources().getString(R.string.message_pass_false),
+                            R.drawable.rat48);
+                    myAlert.myDialog();
+
+                } else if (passwordString.equals(loginString[3])) {
+                    //Password True
+                }else if (passwordString.equals(loginString[3])) {
+
+                    Toast.makeText(MainActivity.this, "Welcom" + loginString[1],
+                            Toast.LENGTH_LONG).show();
+
+
+                } else {
+                    //Password false
+
+                    MyAlert myAlert = new MyAlert(MainActivity.this,
+                            getResources().getString(R.string.title_user_fale),
+                            getResources().getString(R.string.message_user_false),
+                            R.drawable.rat48);
+                    myAlert.myDialog();
+
+                }
 
             } catch (Exception e) {
                 Log.d("14devV2", "e Main ==> " + e.toString());
